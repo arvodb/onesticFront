79a3cfa17062874bd269bc7042672166206864d3
arvodb <daboma@floridauniversitaria.com>
2023-01-25 22:25:35 +0100

    initial setup

---
+++ setup/pseudo.js
@@ -0,0 +1,19 @@
+/* 
+
+TODO =>
+    1.- Realizar un mockup de como va a ser la app
+    2.- Desarrollar diseño bootstrap HTML / CSS
+    3.- Conceptualizar como van a organizarse las diferentes vistas y componentes.
+    4.- Desarrollar interfaces necesarias para las consultas a la API.
+    5.- Organización de las diferentes consultas.
+    6.- Desarrollar componentes / vistas necesarias.
+
+
+?Requisitos resumen =>
+    - Listado pokemon => Vista cuadrícula / lista ;
+        	          => paginación
+    - Detalle pokemon => exp, tipo, ataque, defensa, at.especial, df.especial, ps...
+    - Debe tener modo oscuro;
+    - Filtro favoritos => Necesario investigar.
+*/
+
